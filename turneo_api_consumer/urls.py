9a657c8bf135a792c008e06f4763c0fc5093a9f5
Marko <marko.sandrk1@gmail.com>
2025-04-05 00:00:02 +0200

    Change endpoint to enable API versioning

--- turneo_api_consumer/urls.py
+++ turneo_api_consumer/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("currency-converter", include("currency_converter.urls")),
+    path("api/v1/currency-converter", include("currency_converter.urls")),
     path("admin/", admin.site.urls),
 ]
